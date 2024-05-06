bfa34c5c9eb4df24d0a5da25a5c5059a7c70c87f
Wei Wang <wei@fastmail.com>
2024-05-06 14:57:03 -0400

    Add 'toYAML' function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { stringify } from 'yaml';
 import type { Job } from './Job';
 import type { GlobalKeywords, YAMLHeader } from './Pipeline';
 
@@ -6,3 +7,17 @@ export type GitlabCIDotYAML = {
 	globalKeywords: GlobalKeywords;
 	jobs: Record<string, Job>;
 };
+
+export function toYAML(pipeline: GitlabCIDotYAML) {
+	let result = '';
+
+	if (pipeline.header) {
+		result += stringify(pipeline.header);
+		result += '---\n\n';
+	}
+
+	result += stringify(pipeline.globalKeywords);
+	result += stringify(pipeline.jobs);
+
+	return result;
+}
