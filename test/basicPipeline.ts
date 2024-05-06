53de03707d92df065901fbb65ae1cdfb26a9f6a5
Wei Wang <wei@fastmail.com>
2024-05-06 15:53:49 -0400

    Add unit test

--- test/basicPipeline.ts
+++ test/basicPipeline.ts
@@ -1,23 +1,33 @@
-import type { Pipeline } from '../dist/index';
+import assert from 'node:assert/strict';
+import fs from 'node:fs';
+import test from 'node:test';
+import { toYAML, type Pipeline } from '../dist/index';
 
-export const basicPipeline: Pipeline = {
-	globalKeywords: {
-		workflow: {
-			name: 'Basic Pipeline',
+test('basic pipeline', () => {
+	const basicPipeline: Pipeline = {
+		globalKeywords: {
+			workflow: {
+				name: 'Basic Pipeline',
+			},
+			stages: ['test', 'build'],
+			default: {
+				image: 'node:20.12.2',
+			},
 		},
-		stages: ['test', 'build'],
-		default: {
-			image: 'node:20.12.2',
+		jobs: {
+			job1: {
+				stage: 'test',
+				script: ['echo "it works!"'],
+			},
+			job2: {
+				stage: 'build',
+				script: ['echo "it has been built!"'],
+			},
 		},
-	},
-	jobs: {
-		job1: {
-			stage: 'test',
-			script: ['echo "it works!'],
-		},
-		job2: {
-			stage: 'build',
-			script: ['echo "it has been built!'],
-		},
-	},
-};
+	};
+
+	assert.equal(
+		toYAML(basicPipeline),
+		fs.readFileSync('./test/basicPipeline.yaml', 'utf-8'),
+	);
+});
