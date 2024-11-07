d3a3324fdf999e954ad633fadb53c4a7741eaccf
jade.ahking <jade.ahking@mavenclinic.com>
2024-11-07 17:53:11 -0500

    Run npx @biomejs/biome format --write .

--- src/Input.ts
+++ src/Input.ts
@@ -1,9 +1,9 @@
 export type Input =
 	| string
-    | number
-    | boolean
-    | string[]
-    | Record<string,any>[]
+	| number
+	| boolean
+	| string[]
+	| Record<string, any>[]
 	| (string | Record<string, string | string[]>)[] // this is not documented but it works
 	| {
 			default?: string | string[] | Record<string, string | string[]>;
