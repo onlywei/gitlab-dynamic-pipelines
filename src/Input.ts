875bcc6262b137ca0ce8b3ed0bf04640060c8e4d
Wei Wang <wei@fastmail.com>
2024-11-07 17:37:13 -0500

    Allow more types for Input

--- src/Input.ts
+++ src/Input.ts
@@ -1,7 +1,8 @@
 export type Input =
 	| string
+	| (string | Record<string, string | string[]>)[] // this is not documented but it works
 	| {
-			default?: string;
+			default?: string | string[] | Record<string, string | string[]>;
 			description?: string;
 			options?: string[];
 			regex?: string;
