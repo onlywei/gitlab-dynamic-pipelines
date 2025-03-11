ff728e8f5c6577eff546c098845f4115ce2f7145
Wei Wang <wei@fastmail.com>
2025-03-11 19:15:00 -0400

    Allow cache to be set to an empty array, not object
    This is consistent with the Gitlab docs:
    https://docs.gitlab.com/ci/caching/#disable-cache-for-specific-jobs

--- src/Job.ts
+++ src/Job.ts
@@ -133,7 +133,7 @@ export type Job = {
   allow_failure?: boolean | { exit_codes: number | number[] };
   artifacts?: ArtifactsConfig;
   before_script?: Script;
-  cache?: JobCache | Record<string, never>;
+  cache?: JobCache | [];
   coverage?: `/${string}/`;
   dast_configuration?: { site_profile?: string; scanner_profile?: string };
   dependencies?: string[];
